3213e44e1d659d9af4d182cc3b707ebcbbe003e5
not_a_seagull <notaseagull048@gmail.com>
2020-11-14 22:42:13 -0800

    Add player

---
+++ core/include/player.h
@@ -0,0 +1,32 @@
+/* core/include/player.h 
+ *
+ * Clear Zone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.  
+ *
+ * Clear Zone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Clear Zone.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef CZC_PLAYER_H
+#define CZC_PLAYER_H
+
+#include "entity.h"
+
+#define PLAYER_SUBCLASS 23
+
+class Player: Entity {
+  public:
+    Player();
+    Player(string s, int heal, int intel, int charis, int dex, int wisdom,
+               int constit, double str, long sex, int num);
+};
+
+#endif
